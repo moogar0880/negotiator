ade6afef5fa7d233950724ea333cf900be67e0f6
moogar0880 <moogar0880@gmail.com>
2016-12-07 00:09:11 -0500

    add package doc

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package negotiator is a content negotiation library aimed to support strong
+// content typing for RESTful HTTP services. This library implements both
+// Accept and Content-Type header parsers and struct variants that are fully
+// compliant with both RFC-6839 and RFC-7231.
+package negotiator
